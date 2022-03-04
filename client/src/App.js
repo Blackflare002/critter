f81c679b0411e8f8c092464cd471fc6ac44f5da1
Shane Phillips <shane@loongphillips.com>
2022-03-04 13:56:49 -0500

    Set this up, despite unhelpful directions

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import catLogo from "./cat-logo.svg";
+
+import React from "react";
+
+const App = () => {
+  return <div>Hello world</div>;
+};
+
+export default App;
+
+// import logo from "./logo.svg";
+// import "./App.css";
+// import catLogo from "./cat-logo.svg";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
