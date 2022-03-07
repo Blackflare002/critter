93107de7ca1f65688a5da6d3dcd0e5f8bed44050
Shane Phillips <shane@loongphillips.com>
2022-03-07 17:04:14 -0500

    incremental progress?

--- client/src/components/HomeFeed.js
+++ client/src/components/HomeFeed.js
@@ -1,7 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 const HomeFeed = () => {
+  useEffect(() => {
+    fetch("/api/me/home-feed")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setCurrentUser(data);
+        // setStatus("idle");
+      });
+  }, []);
   return (
     <>
       <Wrapper>Home Feed</Wrapper>
