bc581582edc1b91b26bfa2e8a0dc8c14797a29b2
Shane Phillips <shane@loongphillips.com>
2022-03-08 17:12:10 -0500

    profile content complete

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Wrapper } from "./HomeFeed";
 import CurrentUserContext from "./CurrentUserContext";
+import moment from "moment";
 
 const Profile = () => {
   const { currentUser } = useContext(CurrentUserContext);
@@ -15,10 +16,20 @@ const Profile = () => {
     return (
       <Wrapper>
         <div>Profile</div>
+        <img src={currentUser.profile.bannerSrc} />
+        <img src={currentUser.profile.avatarSrc} />
         <div>Handle: {currentUser.profile.handle}</div>
         <div>Display Name: {currentUser.profile.displayName}</div>
         <div>Location: {currentUser.profile.location}</div>
+        <div>
+          Joined: {moment(currentUser.profile.joined).format("MMMM, YYYY")}
+        </div>
         <div>Bio: {currentUser.profile.bio}</div>
+        <div>Following {currentUser.profile.numFollowing}</div>
+        <div>Followers: {currentUser.profile.numFollowers}</div>
+        <div>Likes: {currentUser.profile.numLikes}</div>
+        <div>Follows you: {currentUser.profile.isFollowingYou}</div>
+        <div>Following: {currentUser.profile.isBeingFollowedByYou}</div>
         {/* <div>{currentUser.profile.handle}</div> */}
       </Wrapper>
     );
